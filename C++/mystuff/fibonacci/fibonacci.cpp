b3d4e41dfa4355bc158f35650cb104c373581a40
AJAUDET <150381581+AJAUDET@users.noreply.github.com>
2024-09-01 12:12:58 -0400

    Add files via upload

---
+++ C++/mystuff/fibonacci/fibonacci.cpp
@@ -0,0 +1,18 @@
+/*
+    makin the fibonacci sequence
+*/
+
+#include <iostream>
+using namespace std;
+
+long fibonacci(int start);
+
+int main()
+{
+   
+}
+
+long fibonacci(int start)
+{
+    
+}
